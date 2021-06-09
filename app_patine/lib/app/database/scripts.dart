bd5e943ea10798ffc5a445a63c83de0f10ff427f
angelMizutani <angelisayuri@yahoo.com.br>
2021-06-09 20:05:38 -0300

    persistencia local

---
+++ app_patine/lib/app/database/scripts.dart
@@ -0,0 +1,31 @@
+final createTable = '''
+  CREATE TABLE usuario(
+    id INT NOT NULL PRIMARY KEY
+    , nome VARCHAR(255) NOT NULL
+    , cpf CHAR(11) NOT NULL
+    , email VARCHAR(255) NOT NULL
+    , login VARCHAR(255) NOT NULL
+    , senha VARCHAR(255) NOT NULL
+    , tipo CHAR(1) NOT NULL
+  )
+''';
+
+final insertTreinador1 = '''
+  INSERT INTO usuario(nome, cpf, email, login, senha, tipo)
+  VALUES('Luara', '12345678900', 'luara@email.com', 'luara_adreninline', '12345', 'T')
+''';
+
+final insertTreinador2 = '''
+  INSERT INTO usuario(nome, cpf, email, login, senha, tipo)
+  VALUES('Asha', '45678912300', 'asha@email.com', 'asha_skatefresh', '12345', 'T')
+''';
+
+final insertAprendiz1 = '''
+  INSERT INTO usuario(nome, cpf, email, login, senha, tipo)
+  VALUES('Angelica', '78912345600', 'angelica@email.com', 'angelica', '12345', 'A')
+''';
+
+final insertAprendiz2 = '''
+  INSERT INTO usuario(nome, cpf, email, login, senha, tipo)
+  VALUES('Mateus', '15947826300', 'mateus@email.com', 'mateus', '12345', 'A')
+''';
--- app_patine/lib/main.dart
+++ app_patine/lib/main.dart
@@ -2,6 +2,27 @@ import 'package:flutter/material.dart';
 
 import 'app/my_app.dart';
 
-void main() {
+/*void main() {
   runApp(MyApp());
+}
+*/
+void main() {
+  demorar();
+  esperarMensagem();
+  print('Olá1');
+  print('Olá2');
+  print('Olá3');
+}
+
+Future imprimirMensagem() {
+  return Future.delayed(Duration(seconds: 3), () => print('Cheguei!'));
+}
+
+esperarMensagem() async {
+  await imprimirMensagem();
+  print('Esperando...');
+}
+
+Future demorar() {
+  return Future.delayed(Duration(seconds: 5), () => print('Desculpe o atraso'));
 }
