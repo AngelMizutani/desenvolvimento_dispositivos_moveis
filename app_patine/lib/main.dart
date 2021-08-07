420c881d36f7d25dd4c222e2e159f9a9e457245e
angelMizutani <angelisayuri@yahoo.com.br>
2021-08-07 14:36:51 -0300

    configuração Firestore

--- app_patine/lib/main.dart
+++ app_patine/lib/main.dart
@@ -1,9 +1,27 @@
 //@dart=2.9
 import 'package:app_patine/app/injection.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'app/my_app.dart';
 
-void main() {
+void main() async {
   setupInjection();
   runApp(MyApp());
+
+  await Firebase.initializeApp();
+
+  FirebaseFirestore.instance.collection('usuario').doc().set({
+    'nome': 'Maria da Silva',
+    'cpf': '123.456.789-00',
+    'email': 'maria@email.com',
+    'login': 'maria_silva',
+    'senha': '123456',
+    'tipo': 'T'
+  });
+
+  FirebaseFirestore.instance
+      .collection('exercicio')
+      .doc()
+      .set({'nome': 'limões', 'descricao': 'desenhe circulos com os patins'});
 }
